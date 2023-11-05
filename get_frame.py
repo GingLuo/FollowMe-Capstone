b0fe7bd5b0b01e2c2ae27f8cee8f9abbc59a5769
Jeffery Cao <75282529+JCao-CMU@users.noreply.github.com>
2023-11-05 01:20:44 -0400

    CHANGE IN GET_FRAME

--- get_frame.py
+++ get_frame.py
@@ -1,95 +1,151 @@
 import pyrealsense2.pyrealsense2 as rs
-# import aspose.threed as a3d
 import numpy as np
 import cv2
 
-is_on = False
-pipeline = None
-depth_scale = None
+class Camera:
+    def __init__(self) -> None:
+        self.counter = 0
+        self.width = 640
+        self.height = 480
 
-def get_frame():
-    global is_on
-    global pipeline
-    if not is_on:
-        setup_camera()
+        self.pc = rs.pointcloud()
+        self.points = rs.points()
 
-    frames = pipeline.wait_for_frames()
-    depth_frame = frames.get_depth_frame()
-    color_frame = frames.get_color_frame()
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+        self.config.enable_stream(rs.stream.depth, self.width, self.height, rs.format.z16, 30)
+        self.config.enable_stream(rs.stream.color, self.width, self.height, rs.format.bgr8, 30)
+        
+        self.profile = self.pipeline.start(self.config)
 
-    # convert images to numpy arrays
-    depth_image = np.asanyarray(depth_frame.get_data())
-    #print("The size of depth_image is:", depth_image.shape)
-    color_image = np.asanyarray(color_frame.get_data())
-    #print("The size of color_image is:", color_image.shape)
-    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        self.depth_sensor = profile.get_device().first_depth_sensor()
+        self.depth_scale = self.depth_sensor.get_depth_scale()
+        # profile = pipeline.get_active_profile()
+        # sensor = profile.get_device().query_sensors()[0]  # 0 for depth sensor, 1 for camera sensor
+        # sensor.set_option(rs.option.max_distance, 0)
+        # sensor.set_option(rs.option.enable_max_usable_range, 0)
+        self.colorizer = rs.colorizer()
+        self.colorizer.set_option(rs.option.visual_preset, 2)
+        print("Depth Scale is: ", self.depth_scale)
+        self.depth_sensor.set_option(rs.option.laser_power, 100)
+        self.laser_pwr = self.depth_sensor.get_option(rs.option.laser_power)
+        print("laser power = ", self.laser_pwr)
 
-    # depth = depth_image[320,240].astype(float)*depth_scale
+        self.laser_range = self.depth_sensor.get_option_range(rs.option.laser_power)
+        print("laser power range = " , self.laser_range.min , "~", self.laser_range.max)
 
-    return (color_image, depth_image, depth_colormap)
+    def get_pic():
+        frames = self.pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
 
+        # convert images to numpy arrays
+        depth_image = np.asanyarray(depth_frame.get_data())
+        #print("The size of depth_image is:", depth_image.shape)
+        color_image = np.asanyarray(color_frame.get_data())
+        #print("The size of color_image is:", color_image.shape)
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+        RGB_path = "runtime_pic/RGB_"+str(self.counter)+".jpg"
+        depth_colormap_path = "runtime_depthpic/depthpic_"str(self.depth_scale)+"_"+str(self.counter)+".jpg"
+        depth_map_path = "runtime_depth/depth_"str(self.depth_scale)+"_"+str(self.counter)+".txt"
+
+        cv2.imwrite(RGB_path, color_image)
+        cv2.imwrite(depth_colormap_path, depth_colormap)
+        np.savetxt(depth_map_path, depth_image)
+        # depth = depth_image[320,240].astype(float)*depth_scale
+        self.counter += 1
+        self.counter %= 60
+        return (RGB_path, depth_colormap_path, depth_map_path) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def get_frame():
+#     global is_on
+#     global pipeline
+#     if not is_on:
+#         setup_camera()
+
+#     frames = pipeline.wait_for_frames()
+#     depth_frame = frames.get_depth_frame()
+#     color_frame = frames.get_color_frame()
+
+#     # convert images to numpy arrays
+#     depth_image = np.asanyarray(depth_frame.get_data())
+#     #print("The size of depth_image is:", depth_image.shape)
+#     color_image = np.asanyarray(color_frame.get_data())
+#     #print("The size of color_image is:", color_image.shape)
+#     depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+#     # depth = depth_image[320,240].astype(float)*depth_scale
+
+#     return (color_image, depth_image, depth_colormap)
+
+# def get_pic():
+#     global is_on
+#     global pipeline
+#     if not is_on:
+#         setup_camera()
+
+#     frames = pipeline.wait_for_frames()
+#     depth_frame = frames.get_depth_frame()
+#     color_frame = frames.get_color_frame()
+
+#     # convert images to numpy arrays
+#     depth_image = np.asanyarray(depth_frame.get_data())
+#     #print("The size of depth_image is:", depth_image.shape)
+#     color_image = np.asanyarray(color_frame.get_data())
+#     #print("The size of color_image is:", color_image.shape)
+#     depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+#     cv2.imwrite("runtime_pic/")
+#     # depth = depth_image[320,240].astype(float)*depth_scale
+
+#     return (color_image, depth_image, depth_colormap)    
+
+# def setup_camera():
+#     global pipeline
+#     global depth_scale
+#     global is_on
+#     #TODO: CHange here
     
 
-def setup_camera():
-    global pipeline
-    global depth_scale
-    global is_on
-    #TODO: CHange here
-    width = 640
-    height = 480
-
-    pc = rs.pointcloud()
-    points = rs.points()
-
-    pipeline = rs.pipeline()
-    config = rs.config()
-    config.enable_stream(rs.stream.depth, width, height, rs.format.z16, 30)
-    config.enable_stream(rs.stream.color, width, height, rs.format.bgr8, 30)
-    #device_product_line = str(device.get_info(rs.camera_info.product_line))
-    #print(device_product_line)
-    
-    profile = pipeline.start(config)
-
-    depth_sensor = profile.get_device().first_depth_sensor()
-    depth_scale = depth_sensor.get_depth_scale()
-    # profile = pipeline.get_active_profile()
-    # sensor = profile.get_device().query_sensors()[0]  # 0 for depth sensor, 1 for camera sensor
-    # sensor.set_option(rs.option.max_distance, 0)
-    # sensor.set_option(rs.option.enable_max_usable_range, 0)
-    colorizer = rs.colorizer()
-    colorizer.set_option(rs.option.visual_preset, 2)
-    print("Depth Scale is: ", depth_scale)
-    depth_sensor.set_option(rs.option.laser_power, 100)
-    laser_pwr = depth_sensor.get_option(rs.option.laser_power)
-    print("laser power = ", laser_pwr)
-
-    laser_range = depth_sensor.get_option_range(rs.option.laser_power)
-    print("laser power range = " , laser_range.min , "~", laser_range.max)
-
-    is_on = True
-
-if __name__ == "__main__":
-    count = 0
-    try:
-        while True:
-            color_image, depth_image, depth_colormap = get_frame()
-            depth = depth_image[320,240].astype(float)*depth_scale
+# if __name__ == "__main__":
+#     count = 0
+#     try:
+#         while True:
+#             color_image, depth_image, depth_colormap = get_frame()
+#             depth = depth_image[320,240].astype(float)*depth_scale
             
-            cv2.imshow('rgb', color_image)
-            cv2.imshow('depth', depth_colormap)
-            count += 1
-            if (count % 100 == 0):
-                cv2.imwrite("test/color_image"+str(count//100)+ ".jpg", color_image)
-                cv2.imwrite("test/depth"+ str(depth_scale) + "_image"+str(count//100)+ ".jpg", depth_colormap)
-                np.savetxt("test/test_depth" + str(depth_scale) +"_image" + str(count//100)+".txt", depth_image)
-                print("Stored!")
-                print(f'Depth: {depth} m')
-                print(f'Depth_scale:{depth_scale} m')
-            if cv2.waitKey(1) == ord("q"):
-               break  
-    except KeyboardInterrupt:
-        pipeline.stop()
-        exit()
+#             cv2.imshow('rgb', color_image)
+#             cv2.imshow('depth', depth_colormap)
+#             count += 1
+#             if (count % 100 == 0):
+#                 cv2.imwrite("test/color_image"+str(count//100)+ ".jpg", color_image)
+#                 cv2.imwrite("test/depth"+ str(depth_scale) + "_image"+str(count//100)+ ".jpg", depth_colormap)
+#                 np.savetxt("test/test_depth" + str(depth_scale) +"_image" + str(count//100)+".txt", depth_image)
+#                 print("Stored!")
+#                 print(f'Depth: {depth} m')
+#                 print(f'Depth_scale:{depth_scale} m')
+#             if cv2.waitKey(1) == ord("q"):
+#                break  
+#     except KeyboardInterrupt:
+#         pipeline.stop()
+#         exit()
 # try:
 #     while True:
 #         frames = pipeline.wait_for_frames()
